03b428813e1d8c10c3eb4a1f91ab065c7b7d14e1
nazurudeen <nazurudeen@yahoo.com>
2021-07-09 11:51:06 +0530

    spliting array

--- splitArray.js
+++ splitArray.js
@@ -1,7 +1,7 @@
 function chunkArray(array, size) {
-  debugger
   let result = []
   for (let i = 0; i < array.length; i += size) {
+    //using slice method
     let chunk = array.slice(i, i + size)
     result.push(chunk)
   }
