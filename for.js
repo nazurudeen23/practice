5e666824a5a365b274df7909f5addaf11aba4e1e
nazurudeen <nazurudeen@yahoo.com>
2021-07-17 01:51:18 +0530

    for loop

---
+++ for.js
@@ -0,0 +1,14 @@
+const arr = [3, 5, 7];
+arr.foo = 'hello';
+
+for (let i in arr) {
+   console.log(i); // logs "0", "1", "2", "foo"
+}
+
+for (let i of arr) {
+   console.log(i); // logs 3, 5, 7
+}
+
+for(let i of arr.foo){
+  console.log(i); // logs "h","e","l","l","o"
+}
